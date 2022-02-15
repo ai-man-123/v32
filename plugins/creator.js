ec8c216c71405fe7f6908348a086e58f31e180ba
AIMAN *͘⁴̅⁰͍⁴̵〆* <82744566+ai-man-123@users.noreply.github.com>
2022-02-15 12:54:10 +0800

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -10,7 +10,7 @@ let list = []
         "displayName": `${list.length} Contact`,
         "contacts": list 
         }, 'contactsArrayMessage', { quoted: m })
-        test.reply('Tuh Orang Gamteng>3')
+        test.reply('Tuh Orang Gamteng>3/owner')
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
